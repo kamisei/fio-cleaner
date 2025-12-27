d2e389f77e2907fa74a24240f2ed74509476be3e
Алёна Голяновская <kamisei@MacBook-Pro-Elena.local>
2025-12-27 23:07:17 +0100

    Configure media storage

--- src/config/settings.py
+++ src/config/settings.py
@@ -116,3 +116,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+from pathlib import Path
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
